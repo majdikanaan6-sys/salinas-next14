3ab34dca0702b03af8c2e329ce6c2d9dc1890f12
Majdi Kanaan <majdikanaan6@gmail.com>
2025-11-12 11:20:46 -0800

    fix es

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react'
 
 export default function Home() {
   useEffect(() => {
-    const lang = (navigator.language || navigator['userLanguage'] || 'en').slice(0,2)
+    const lang = (navigator.language || 'en').slice(0, 2);
     const supported = ['en','es','fr','ar']
     const target = supported.includes(lang) ? lang : 'en'
     window.location.replace('/' + target)
